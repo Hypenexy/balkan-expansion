a9d36811a90710c5c2c837124d30fa5ea8e90228
Snow <denisterabg@gmail.com>
2025-12-03 19:15:46 +0200

    Added Lapis Sword as an item

--- src/main/java/net/georgidenis/balkanexpansion/item/ModItems.java
+++ src/main/java/net/georgidenis/balkanexpansion/item/ModItems.java
@@ -12,6 +12,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item DONER = registerItem("doner", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID,"doner")))));
+    public static final Item LAPIS_SWORD = registerItem("lapis_sword", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID,"lapis_sword")))));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(BalkanExpansion.MOD_ID, name), item);
@@ -22,6 +23,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(DONER);
+            entries.add(LAPIS_SWORD);
         });
     }
 }
