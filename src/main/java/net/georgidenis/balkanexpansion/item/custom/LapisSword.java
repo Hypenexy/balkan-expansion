d08ad8ad799053c1b9c3dcb71ae3aa6f8f1f0b90
Snow <denisterabg@gmail.com>
2025-12-04 01:15:27 +0200

    Added (NOT WORKING) custom logic to the mod

---
+++ src/main/java/net/georgidenis/balkanexpansion/item/custom/LapisSword.java
@@ -0,0 +1,12 @@
+package net.georgidenis.balkanexpansion.item.custom;
+
+import net.minecraft.component.type.WeaponComponent;
+import net.minecraft.item.Item;
+
+public class LapisSword extends Item {
+    public LapisSword(Settings settings, WeaponComponent weapon) {
+        super(settings);
+    }
+
+
+}
