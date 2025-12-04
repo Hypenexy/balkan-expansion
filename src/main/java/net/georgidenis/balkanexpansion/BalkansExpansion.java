2f577d1d6217ced9ecbf15eee0e03f2c292e2f7b
Hypenexy <61563710+Hypenexy@users.noreply.github.com>
2025-12-04 16:39:03 +0200

    Re-added a Creative Tab and Gyroman structure and added 2 new paintings

--- src/main/java/net/georgidenis/balkanexpansion/BalkansExpansion.java
+++ src/main/java/net/georgidenis/balkanexpansion/BalkansExpansion.java
@@ -1,5 +1,6 @@
 package net.georgidenis.balkanexpansion;
 
+import net.georgidenis.balkanexpansion.item.ModCreativeTab;
 import net.georgidenis.balkanexpansion.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -33,6 +34,9 @@ public class BalkansExpansion {
         // Note that this is necessary if and only if we want *this* class (BalkansExpansion) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        // Registering the creative mode tab menu
+        ModCreativeTab.register(modEventBus);
 
         // Adding the register in our main folder
         ModItems.register(modEventBus);
---
+++ src/main/java/net/georgidenis/balkanexpansion/item/ModCreativeTab.java
@@ -0,0 +1,28 @@
+package net.georgidenis.balkanexpansion.item;
+
+import net.georgidenis.balkanexpansion.BalkansExpansion;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BalkansExpansion.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BALKANS_ITEMS_TAB = CREATIVE_MODE_TAB.register("balkans_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.DONER.get()))
+                    .title(Component.translatable("itemgroup.balkansexpansion.the_balkans_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.DONER);
+                        output.accept(ModItems.NECROMANCER_SCROLL);
+                    }).build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
