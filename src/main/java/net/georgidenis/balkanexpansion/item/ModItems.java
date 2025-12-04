cc7cc875782d8305de330e17f713954079ae6c7a
datanotsecure <abnormalcodes@gmail.com>
2025-12-03 20:16:04 -0800

    Fixed Lapis Sword logic, now it acts like a sword duh

--- src/main/java/net/georgidenis/balkanexpansion/item/ModItems.java
+++ src/main/java/net/georgidenis/balkanexpansion/item/ModItems.java
@@ -4,17 +4,66 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 import net.georgidenis.balkanexpansion.BalkanExpansion;
 import net.georgidenis.balkanexpansion.block.ModBlocks;
+import net.georgidenis.balkanexpansion.item.custom.LapisSword;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.*;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item DONER = registerItem("doner", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "doner"))).food(ModFoodComponents.DONER)));
-    public static final Item LAPIS_SWORD = registerItem("lapis_sword", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "lapis_sword")))));
+    //public static final Item LAPIS_SWORD = registerItem("lapis_sword", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "lapis_sword")))));
+    public static final Item LAPIS_SWORD = registerItem("lapis_sword",
+            new Item(new Item.Settings()
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM,
+                            Identifier.of(BalkanExpansion.MOD_ID, "lapis_sword")))
+                    .maxCount(1) // <-- stacking
+                    .component(DataComponentTypes.WEAPON,
+                            new WeaponComponent(
+                                    6,      // damage
+                                    -2.4f   // speed
+                            )
+                    )
+                    .component(DataComponentTypes.ATTRIBUTE_MODIFIERS,
+                            AttributeModifiersComponent.builder()
+                                    .add(
+                                            EntityAttributes.ATTACK_DAMAGE,
+                                            new EntityAttributeModifier(
+                                                    Identifier.of("lapis_sword_attack"),
+                                                    6.0,
+                                                    EntityAttributeModifier.Operation.ADD_VALUE
+                                            ),
+                                            AttributeModifierSlot.MAINHAND
+                                    )
+                                    .add(
+                                            EntityAttributes.ATTACK_SPEED,
+                                            new EntityAttributeModifier(
+                                                    Identifier.of("lapis_sword_speed"),
+                                                    -2.4,
+                                                    EntityAttributeModifier.Operation.ADD_VALUE
+                                            ),
+                                            AttributeModifierSlot.MAINHAND
+                                    )
+                                    .build()
+                    )
+
+                    .component(DataComponentTypes.MAX_DAMAGE, 500) // durability
+                    .component(DataComponentTypes.DAMAGE, 0)
+            )
+
+    );
+
     public static final Item RAW_BRONZE = registerItem("raw_bronze", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "raw_bronze")))));
     public static final Item BRONZE_HELMET = registerItem("bronze_helmet", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "bronze_helmet")))));
     public static final Item NECROMANCER_SCROLL = registerItem("necromancer_scroll", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BalkanExpansion.MOD_ID, "necromancer_scroll")))));
--- src/main/java/net/georgidenis/balkanexpansion/item/custom/LapisSword.java
+++ src/main/java/net/georgidenis/balkanexpansion/item/custom/LapisSword.java
@@ -1,12 +1,29 @@
 package net.georgidenis.balkanexpansion.item.custom;
 
-import net.minecraft.component.type.WeaponComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class LapisSword extends Item {
-    public LapisSword(Settings settings, WeaponComponent weapon) {
+
+    public LapisSword(Settings settings) {
         super(settings);
     }
 
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
 
+        // swing animation + print message in console
+        if (!world.isClient()) {
+            System.out.println("Lapis me4!");
+        }
+
+        user.swingHand(hand);
+
+        return ActionResult.SUCCESS;
+    }
 }
